fed6d355750067a41e06108d11854488aebd7914
javier sicardi <javisicardi94@gmail.com>
2020-08-19 17:58:28 -0300

    Agregadas migraciones

---
+++ am-pm-solutions.DAL/Migrations/202008192052298_init.cs
@@ -0,0 +1,45 @@
+﻿namespace am_pm_solutions.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tblBolsaTrabajo",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Fecha = c.DateTime(nullable: false),
+                        NombreApellido = c.String(nullable: false, maxLength: 100),
+                        Localidad = c.String(nullable: false, maxLength: 100),
+                        Provincia = c.String(nullable: false, maxLength: 100),
+                        Email = c.String(nullable: false, maxLength: 80),
+                        NombreArchivoCV = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tblContactos",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FechaContacto = c.DateTime(nullable: false),
+                        NombreApellido = c.String(nullable: false, maxLength: 100),
+                        Telefono = c.String(nullable: false, maxLength: 50),
+                        Email = c.String(nullable: false, maxLength: 80),
+                        Mensaje = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.tblContactos");
+            DropTable("dbo.tblBolsaTrabajo");
+        }
+    }
+}
---
+++ am-pm-solutions.DAL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace am_pm_solutions.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<am_pm_solutions.DAL.AmPmContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(am_pm_solutions.DAL.AmPmContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
