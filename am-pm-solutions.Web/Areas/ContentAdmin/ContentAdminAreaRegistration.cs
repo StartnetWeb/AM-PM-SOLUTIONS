169a3cabbe1c53fe1a566b4b51dfae31ca88d86a
javisicardi <javisicardi94@gmail.com>
2020-08-20 11:20:00 -0300

    Recfactorizado proyecto .Web

---
+++ am-pm-solutions.Web/Areas/ContentAdmin/ContentAdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace am_pm_solutions.Web.Areas.ContentAdmin
+{
+    public class ContentAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ContentAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ContentAdmin_default",
+                "ContentAdmin/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "am-pm-solutions.Web.Areas.ContentAdmin.Controllers" }
+            );
+        }
+    }
+}
--- am-pm-solutions/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="am_pm_solutions.MvcApplication" Language="C#" %>
