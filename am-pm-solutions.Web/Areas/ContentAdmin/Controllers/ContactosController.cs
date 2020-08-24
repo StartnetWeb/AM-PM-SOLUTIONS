3b074296d8c25af458eb1663a2a5a7f25c202a26
javier sicardi <javisicardi94@gmail.com>
2020-08-24 17:06:48 -0300

    Maquetada seccion contactos admin

---
+++ am-pm-solutions.Web/Areas/ContentAdmin/Controllers/ContactosController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using am_pm_solutions.DAL;
+using am_pm_solutions.Entities;
+using PagedList;
+
+namespace am_pm_solutions.Web.Areas.ContentAdmin.Controllers
+{
+    [Authorize]
+    public class ContactosController : Controller
+    {
+        private AmPmContext db = new AmPmContext();
+
+        // GET: ContentAdmin/Contactos
+        public ActionResult Index(int? page, string searchString, string currentFilter)
+        {
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.currentFilter = searchString;
+            int pageSize = 20;
+            int pageNumber = (page ?? 1);
+            var contactos = db.Contacto.OrderByDescending(x => x.FechaContacto).ToList();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                contactos = contactos.Where(x => x.NombreApellido.ToUpper().Contains(searchString.ToUpper())).ToList();
+            }
+
+            return View(contactos.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: ContentAdmin/Contactos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // GET: ContentAdmin/Contactos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContentAdmin/Contactos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FechaContacto,NombreApellido,Telefono,Email,Mensaje")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacto.Add(contacto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contacto);
+        }
+
+        // GET: ContentAdmin/Contactos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: ContentAdmin/Contactos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FechaContacto,NombreApellido,Telefono,Email,Mensaje")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contacto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contacto);
+        }
+
+        // GET: ContentAdmin/Contactos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: ContentAdmin/Contactos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contacto contacto = db.Contacto.Find(id);
+            db.Contacto.Remove(contacto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
