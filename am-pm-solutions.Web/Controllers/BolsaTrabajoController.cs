dcd9e63140675bc664f3dc05f29c5ea7f8fc9921
javier sicardi <javisicardi94@gmail.com>
2020-09-14 18:18:59 -0300

    Aplicada plantilla en ingles

---
+++ am-pm-solutions.Web/Controllers/BolsaTrabajoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace am_pm_solutions.Web.Controllers
+{
+    public class BolsaTrabajoController : Controller
+    {
+        // GET: BolsaTrabajo
+        public ActionResult IndexEn()
+        {
+            return View();
+        }
+    }
+}
--- am-pm-solutions.Web/Controllers/HomeController.cs
+++ am-pm-solutions.Web/Controllers/HomeController.cs
@@ -8,7 +8,43 @@ namespace am_pm_solutions.Web.Controllers
 {
     public class HomeController : Controller
     {
+        //CONTROLADORES DE VISTAS EN INGLES
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CompanyEn()
+        {
+            return View();
+        }
+
+        public ActionResult ServicesEn()
+        {
+            return View();
+        }
+
+        public ActionResult ServicesProjectMangementEn()
+        {
+            return View();
+        }
+
+        public ActionResult ServicesConsultancyEn()
+        {
+            return View();
+        }
+
+        public ActionResult IndustriesEn()
+        {
+            return View();
+        }
+
+        public ActionResult IndustriesGeneralEn()
+        {
+            return View();
+        }
+
+        public ActionResult IndustriesTraslationEn()
         {
             return View();
         }
