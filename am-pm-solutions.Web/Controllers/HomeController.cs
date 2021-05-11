17f37da6aea2a9c2dde050e251526251aa77a1c0
Leandro Gaido <lgaido34@gmail.com>
2021-05-11 14:54:21 -0300

    metodo exportarDatos

--- am-pm-solutions.Web/Controllers/HomeController.cs
+++ am-pm-solutions.Web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -102,6 +105,36 @@ namespace am_pm_solutions.Web.Controllers
         public ActionResult IndustriesTraslationEs()
         {
             return View();
+        }
+
+        public ActionResult ExportarOdontogramas()
+        {
+            #region Headers
+            var re = Request;
+            var headers = re.Headers;
+            string authorizationHeader = "";
+
+            //Request.Headers.GetValues("authorization");
+            try
+            {
+                authorizationHeader = headers.GetValues("authorization").First();
+            }
+            catch (Exception e)
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
+            #endregion
+            //Prueba 
+            //Produccion ewogICAgIlVzZXIiOiAiQ0FNSSAtIEFTT0MuTVVULkNFTlRSTyBNRURJQ08gU0FOIEZDTy4iLAogICAgIklkIjoiMTI0IiwKICAgICJBY3Rpb24iOiAiR2V0T2RvbnRvZ3JhbWFzIiwKICAgICJTZWNyZXQiOiAiYUhSMGNEb3ZMMjlrYjI1MGIzQnlaWE11WTI5dExtRnlMdz09Igp9
+            if (authorizationHeader == "ewogICAgIlVzZXIiOiAiQ0FNSSAtIEFTT0MuTVVULkNFTlRSTyBNRURJQ08gU0FOIEZDTy4iLAogICAgIklkIjoiMTI0IiwKICAgICJBY3Rpb24iOiAiR2V0T2RvbnRvZ3JhbWFzUHJ1ZWJhIiwKICAgICJTZWNyZXQiOiAiYUhSMGNEb3ZMMjlrYjI1MGIzQnlaWE11WTI5dExtRnlMMUJ5ZFdWaVlRPT0iCn0=")
+            {
+                string path = Server.MapPath("~/Areas/ContentAdmin/Content/ListadoOdontogramas/ListadoOdontograma.txt");
+                return File(path, "text/plain", "ListadoOdontograma.txt");
+            }
+            else
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
